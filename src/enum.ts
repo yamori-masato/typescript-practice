ee94a44b65fc6db504e4be83b6b272d68bf1c678
yamori-masato <yamorimasato@gmail.com>
2020-12-18 00:03:42 +0900

    add src/enum.ts

---
+++ src/enum.ts
@@ -0,0 +1,37 @@
+export {}
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+console.log(Months.January)
+console.log(Months.February)
+console.log(Months.December)
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '#000000',
+}
+
+let green = COLORS.GREEN
+console.log(green)
+
+enum COLOR {
+  YELLOW = '#FFFF00',
+  GRAY = '#808080',
+}
+
+COLOR.YELLOW
