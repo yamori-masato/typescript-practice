138ccb75276de408532c899d54776d20540eb9b9
yamori-masato <yamorimasato@gmail.com>
2020-12-17 23:48:44 +0900

    add src/array.ts

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {}
+
+let numbers: number[] = [1, 2, 3]
+
+let numbers2: Array<number> = [1, 2, 3]
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto']
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'coffeeScript']
+
+let nijigenHairetu: number[][] = [
+  [50, 100],
+  [15, 300],
+]
+
+let hairetu: (number | boolean | string)[] = [1, false, 'Japan']
